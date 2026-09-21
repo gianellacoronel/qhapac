744ecea01f5b84595fa5f9159fc58e57430dbcb7
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-09-21 12:35:50 -0500

    feat: install shadcn/ui

--- qhapac/app/layout.tsx
+++ qhapac/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Source_Sans_3, Raleway } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const ralewayHeading = Raleway({subsets:['latin'],variable:'--font-heading'});
+
+const sourceSans3 = Source_Sans_3({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", sourceSans3.variable, ralewayHeading.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
